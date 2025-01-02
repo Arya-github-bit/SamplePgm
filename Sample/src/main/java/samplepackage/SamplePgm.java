e59ec7aaa3271b899759f410950b33a2a1897007
Radhi <Radhi@192.168.1.6>
2025-01-02 22:10:30 +0530

    push

---
+++ Sample/src/main/java/samplepackage/SamplePgm.java
@@ -0,0 +1,11 @@
+package samplepackage;
+
+public class SamplePgm {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Sample Pgm");
+	}
+
+}
